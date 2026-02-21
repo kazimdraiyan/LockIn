fe7949a746b13a22a82c6811688f596c2a1c7e1d
Typhon13 <90.ahon.sazzad.09@gmail.com>
2026-02-21 12:17:08 +0600

    Add home page version 1

---
+++ src/main/java/app/lockin/lockin/client/controller/HomeController.java
@@ -0,0 +1,17 @@
+package app.lockin.lockin.client.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeController {
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private AnchorPane mainPane;
+    @FXML
+    private AnchorPane lockinPane;
+    @FXML
+    private AnchorPane lockinPane2;
+    @FXML
+    private AnchorPane lockinPane3;
+}
